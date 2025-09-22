df8531406ac2527fefb8a73af6eb936ae238e51d
afanasevv <veiby325@gmail.com>
2025-09-22 17:28:47 +1000

    fix

--- main.go
+++ main.go
@@ -58,6 +58,7 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	fmt.Print(vlessLink)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
